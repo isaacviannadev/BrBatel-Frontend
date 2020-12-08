fc2136fbb19b7279cf9fc57230c5f05c5964f744
Isaac <isaacsvianna@gmail.com>
2020-12-07 21:00:33 -0300

    Add: authentication context (initial)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useRef, useContext } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
 
-
+import AuthContext from "../../context/AuthContext";
 import getValidationErrors from "../../utils/getValidationErrors";
 
 import logoImg from "../../assets/logo.svg";
@@ -17,6 +17,10 @@ import { Container, Content, Background } from "./styles";
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+
+
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
